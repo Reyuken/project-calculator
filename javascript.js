db1de4151ff6d906f01950994c4ce22311e9e58a
Reyuken <212830200+Reyuken@users.noreply.github.com>
2025-07-11 21:46:44 +0800

    Created function display - to display button input to screen Modified period button to call function display

--- javascript.js
+++ javascript.js
@@ -1,66 +1,74 @@
-document.addEventListener("DOMContentLoaded",()=>{
+document.addEventListener("DOMContentLoaded", () => {
 
-// const period = document.getElementById("period");
-// period.addEventListener("click",()=>console.log("."));
+    // const period = document.getElementById("period");
+    // period.addEventListener("click",()=>console.log("."));
 
-const zero = document.getElementById("zero");
-zero.addEventListener("click",()=>console.log("0"));
+    const zero = document.getElementById("zero");
+    zero.addEventListener("click", () => console.log("0"));
 
-const equals = document.getElementById("equals");
-equals.addEventListener("click",()=>console.log("="));
+    const equals = document.getElementById("equals");
+    equals.addEventListener("click", () => console.log("="));
 
-const plus = document.getElementById("plus");
-plus.addEventListener("click",()=>console.log("+"));
+    const plus = document.getElementById("plus");
+    plus.addEventListener("click", () => console.log("+"));
 
-const one = document.getElementById("one");
-one.addEventListener("click",()=>console.log("1"));
+    const one = document.getElementById("one");
+    one.addEventListener("click", () => console.log("1"));
 
-const two = document.getElementById("two");
-two.addEventListener("click",()=>console.log("2"));
+    const two = document.getElementById("two");
+    two.addEventListener("click", () => console.log("2"));
 
-const three = document.getElementById("three");
-three.addEventListener("click",()=>console.log("3"));
+    const three = document.getElementById("three");
+    three.addEventListener("click", () => console.log("3"));
 
-const minus = document.getElementById("minus");
-minus.addEventListener("click",()=>console.log("-"));
+    const minus = document.getElementById("minus");
+    minus.addEventListener("click", () => console.log("-"));
 
-const four = document.getElementById("four");
-four.addEventListener("click",()=>console.log("4"));
+    const four = document.getElementById("four");
+    four.addEventListener("click", () => console.log("4"));
 
-const five = document.getElementById("five");
-five.addEventListener("click",()=>console.log("5"));
+    const five = document.getElementById("five");
+    five.addEventListener("click", () => console.log("5"));
 
-const six = document.getElementById("six");
-six.addEventListener("click",()=>console.log("6"));
+    const six = document.getElementById("six");
+    six.addEventListener("click", () => console.log("6"));
 
-const multiply = document.getElementById("multiply");
-multiply.addEventListener("click",()=>console.log("*"));
+    const multiply = document.getElementById("multiply");
+    multiply.addEventListener("click", () => console.log("*"));
 
-const seven = document.getElementById("seven");
-seven.addEventListener("click",()=>console.log("7"));
+    const seven = document.getElementById("seven");
+    seven.addEventListener("click", () => console.log("7"));
 
-const eight = document.getElementById("eight");
-eight.addEventListener("click",()=>console.log("8"));
+    const eight = document.getElementById("eight");
+    eight.addEventListener("click", () => console.log("8"));
 
-const nine = document.getElementById("nine");
-nine.addEventListener("click",()=>console.log("9"));
+    const nine = document.getElementById("nine");
+    nine.addEventListener("click", () => console.log("9"));
 
-const divide = document.getElementById("divide");
-divide.addEventListener("click",()=>console.log("/"));
+    const divide = document.getElementById("divide");
+    divide.addEventListener("click", () => console.log("/"));
 
-const clear = document.getElementById("clear");
-clear.addEventListener("click",()=>console.log("clear"));
+    const clear = document.getElementById("clear");
+    clear.addEventListener("click", () => console.log("clear"));
 
-const deleteButton = document.getElementById("deleteButton");
-deleteButton.addEventListener("click",()=>console.log("deleteButton"));
-
-// function display(){
+    const deleteButton = document.getElementById("deleteButton");
+    deleteButton.addEventListener("click", () => console.log("deleteButton"));
 
 
-// }
+    function display(buttonInputSign) {
+        const screen = document.querySelector(".screen");
+        const input = document.createElement("div");
+        screen.append(input);
+        input.setAttribute("id", "inputID");
+        input.innerText = buttonInputSign;
+    
+}
 
-// const period = document.getElementById("period");
-// period.addEventListener("click",()=>console.log("."));
+    const period = document.getElementById("period");
+    period.addEventListener("click", ()=>{
+    const buttonInputSign = ".";
+    display(buttonInputSign); 
+    });
 
 
 })
